7a46a040df6ea53cec5df4660061e9ddcf0e012c
MartinVoehringer <Martin.Voehringer@student.reutlingen-university.de>
2017-06-13 20:13:44 +0200

    Added FB Button

--- app/src/main/java/com/bahlot/a4gewinnt/frontend/FourConnect.java
+++ app/src/main/java/com/bahlot/a4gewinnt/frontend/FourConnect.java
@@ -4,10 +4,19 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ViewSwitcher;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.share.model.GameRequestContent;
+import com.facebook.share.widget.GameRequestDialog;
 
 public class FourConnect extends AppCompatActivity implements View.OnClickListener{
     private ViewSwitcher switchScreen;
@@ -23,16 +32,55 @@ public class FourConnect extends AppCompatActivity implements View.OnClickListen
     public Button buttonSinglePStart;
     public Button buttonMultiPStart;
     public Button buttonFacebookAPIConnect;
-
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private GameRequestDialog requestDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_four_connect);
         firstStart();
-
         //setContentView(R.layout.landscape);
         //setContentView(R.layout.portrait);
+        View view = findViewById(R.id.four_connect_layout);
+        loginButton = (LoginButton) view.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Log.v("Lel1", "Success!!!1");
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+        requestDialog = new GameRequestDialog(this);
+        requestDialog.registerCallback(callbackManager,
+                new FacebookCallback<GameRequestDialog.Result>() {
+                    public void onSuccess(GameRequestDialog.Result result) {
+                        //String id = result.getId();
+                        Log.v("FB", "Success invite");
+                    }
+                    public void onCancel() {
+                        Log.v("FB", "Invite canceled");
+                    }
+                    public void onError(FacebookException error) {
+                        Log.v("FB", error.getMessage());
+                    }
+                }
+        );
 
     }
     public void firstStart() {
@@ -65,6 +113,13 @@ public class FourConnect extends AppCompatActivity implements View.OnClickListen
 
     }
 
+    public void onPlayerMultiplayerClicked(View v){
+        GameRequestContent content = new GameRequestContent.Builder()
+                .setMessage("Come play this level with me")
+                .build();
+        requestDialog.show(content);
+    }
+
     @Override
     public void onClick(View v) {
 
@@ -89,5 +144,11 @@ public class FourConnect extends AppCompatActivity implements View.OnClickListen
 
 
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
